bdb2cc9e7c3523e821be0981dae424b884efdef2
tai <zzwzzhao@163.com>
2012-04-02 00:23:06 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "welcome to the Sample App!"
       redirect_to @user
     else
@@ -15,5 +16,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def destroy
   end
 end
